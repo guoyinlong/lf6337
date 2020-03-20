919007146f5f0dcaacf20d8de084ac1ccbdb6f6d
guoyinlong <guoyl@itnova.com.cn>
2020-03-20 13:16:44 +0800

    no message

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+// import { BackTop } from 'antd';
+// import ReactDOM from 'react-dom';
+// import {
+//     BrowserRouter as Router,
+//     Route,
+//     Link
+// }from 'react-router-dom';
+
+import SiderDemo from "./Navi/Navi"
+
+
+
+
+function App() { 
+ 
+  return (
+    <div className="App">
+  <SiderDemo/>
+
+    </div>
+  );
+}
+
+export default App;
